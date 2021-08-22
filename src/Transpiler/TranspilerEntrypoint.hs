01e9c8462c5b1b962eb23eb5d26ccd5c3645853f
Your Name <kevin.kds80@gmail.com>
2021-08-22 11:34:27 -0300

    Refactored Transpiler Entrypoint

--- src/Transpiler/TranspilerEntrypoint.hs
+++ src/Transpiler/TranspilerEntrypoint.hs
@@ -18,18 +18,14 @@ startParsing [path, format] = do
     else do
         contents <- try (readFile path) :: IO (Either SomeException [Char])
         case contents of
-            Right file ->
-                if null file then
-                    putStrLn $ _error "[Transpiler] File is empty"
-                else do
-                    parsingResult <- try (parsingFlowSteps file path format)
-                        :: IO (Either SomeException ())
-                    case parsingResult of
-                        Right file -> do return ()
-                        Left e -> do 
-                            putStrLn (_error $ show e)
-            Left e -> do
-                putStrLn $ _error "[File not found] Error file not found"
+            Left e -> putStrLn $ _error "[File not found] Error file not found"
+            Right []   -> putStrLn $ _error "[Transpiler] File is empty"
+            Right file -> do
+                parsingResult <- try (parsingFlowSteps file path format)
+                    :: IO (Either SomeException ())
+                case parsingResult of
+                    Right _ -> do return ()
+                    Left e  -> do putStrLn (_error $ show e)
 startParsing _ = do
     putStrLn $ _error
         "[CLI - Transpiler] Error expecting filepath and format to be converted"
@@ -39,20 +35,18 @@ parsingFlowSteps program path format = do
     let tokens = startAnalysis program
     let (warnings, ir) = startSemanticAnalysis (snd tokens)
     mapM_ (putStrLn . _warning) warnings
+    
     let pathWithoutExtension = reverse $ drop 5 (reverse path)
     startTranslation ir pathWithoutExtension format
     putStrLn $ _success "Json successfully parsed"
-    return ()
 
 startTranslation :: [[Char]] -> [Char] -> [Char] -> IO ()
-startTranslation ir path "*" = do
+startTranslation ir path "*"   = do
     genJSFile ir path
     genYmlFile ir path
-startTranslation ir path "js" = do
-    genJSFile ir path
-startTranslation ir path "yml" = do
-    genYmlFile ir path
-startTranslation _ _ _ = error
+startTranslation ir path "js"  = do genJSFile ir path
+startTranslation ir path "yml" = do genYmlFile ir path
+startTranslation _ _ _         = error
     "[Transpiler - Translation Format] Error choosed format is not supported"
 
 
